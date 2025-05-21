7d070a8c7175d798f632a48d94071700dbc88481
Rithika P Shetty <rithikashetty356@gmail.com>
2025-05-22 00:38:20 +0530

    Update ColorExtract.java

--- prg1-listinterface/ColorExtract.java
+++ prg1-listinterface/ColorExtract.java
@@ -1,6 +1,7 @@
 /* 4. Write a java program for getting different colors through ArrayList interface and extract the
 elements 1st and 2nd from the ArrayList object by using SubList()
  */
+
 package prg_1b_4;
 import java.util.*;
 public class ColorExtract {
